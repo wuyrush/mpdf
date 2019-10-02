a34c5db315b49d286821f19426244814de383824
doobdoob <5844553+wuyrush@users.noreply.github.com>
2019-10-02 08:53:55 -0700

    YAGNI

--- main.go
+++ main.go
@@ -1,173 +1,206 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"sync"
+	"time"
 
-	ftype "github.com/h2non/filetype"
+	"github.com/h2non/filetype"
 	"github.com/jung-kurt/gofpdf"
 )
 
-func main() {
-	// prepare args
-
-}
-
-type filetype int
-
 const (
-	TypeAbsent filetype = iota
-	TypeFile
-	TypeDir
-	TypeUnknown
-
-	MIMETypePDF       = "application/pdf"
-	HeaderLengthBytes = 261
+	// https://github.com/h2non/filetype
+	MIMETypePDF           = "application/pdf"
+	FileHeaderLengthBytes = 261
 )
 
-// os file system path
-type path string
+func main() {
+	// setup cli
+	in := flag.String("in", ".", "Path to the directory containing PDF files to merge")
+	out := flag.String("out", ".", "Path to the merged PDF file")
+	chrono := flag.Bool("c", false, "Merge PDF files in the order of last modification time")
+	reversed := flag.Bool("r", false, "Reverse merge order")
+	overwrite := flag.Bool("f", false, "Overwrite even if output file already existed")
+	flag.Parse()
 
-// absolute path
-func (p path) abs() (string, error) {
-	abs, err := filepath.Abs(string(p))
+	outAbs, err := filepath.Abs(*out)
 	if err != nil {
-		return "", err
+		fmt.Printf("Error when checking output path %s: %s\n", *out, err)
+		os.Exit(1)
 	}
-	return abs, nil
+	ok, err := canWriteOut(outAbs, *overwrite)
+	if err != nil {
+		fmt.Printf("Error when checking output path %s: %s\n", outAbs, err)
+		os.Exit(1)
+	}
+	if !ok {
+		fmt.Println("Cannot write to output path %s", outAbs)
+		os.Exit(1)
+	}
+	inAbs, err := filepath.Abs(*in)
+	if err != nil {
+		fmt.Printf("Error when checking input path %s: %s\n", *in, err)
+		os.Exit(1)
+	}
+	m := merger{
+		in:        inAbs,
+		out:       outAbs,
+		chrono:    *chrono,
+		reversed:  *reversed,
+		overwrite: *overwrite,
+	}
+	err = m.merge()
+	if err != nil {
+		fmt.Printf("Error when merging PDF files: %s", err)
+		os.Exit(1)
+	}
 }
 
-// path of parent directory
-func (p path) parent() (path, error) {
-	abs, err := p.abs()
-	if err != nil {
-		return path(""), err
+/*
+	Check if we can write to out;
+		1. If out exists and is a file, canWrite = overwrite;
+		2. If out exists and is a directory, canWrite = true. Use a random filename for out in this case
+		3. If out doesn't exist:
+			3.1 If out doesn't exist and its parent exist and the parent is a directory,
+				then canWrite = true. Use a random filename for out in this case;
+			3.2 If out doesn't exist and its parent doesn't exist / its parent exists but a is a non-dir,
+				canWrite = false
+*/
+func canWriteOut(p string, overwrite bool) (bool, error) {
+	info, err := os.Stat(p)
+	isNotExist := os.IsNotExist(err)
+	if err != nil && !isNotExist {
+		return false, err
 	}
-	return path(filepath.Dir(abs)), nil
-}
-
-func (p path) fileType() (filetype, error) {
-	abs, err := p.abs()
-	if err != nil {
-		return TypeUnknown, err
+	if isNotExist {
+		// case 3
+		parent := filepath.Dir(p)
+		info, err := os.Stat(parent)
+		// either the parent dir doesn't exist or any other errors occur, out is not writable anyways
+		if err != nil {
+			return false, err
+		}
+		return info.IsDir(), nil
 	}
-	info, err := os.Stat(abs)
-	if err != nil && os.IsNotExist(err) {
-		return TypeAbsent, nil
-	} else if err != nil {
-		return TypeUnknown, err
-	}
-	ft := TypeFile
 	if info.IsDir() {
-		ft = TypeDir
+		return true, nil
 	}
-	return ft, nil
+	// assume out points to an existing file
+	return overwrite, nil
 }
 
 type merger struct {
-	in        path
-	out       path
-	chrono    bool
+	in        string
+	out       string
 	reversed  bool
+	chrono    bool
 	overwrite bool
 }
 
-// Ok to write to the destination?
-func (m merger) canWriteOut() (bool, error) {
-	ft, err := m.out.fileType()
+/*
+	Then collect files which we believe are of type PDF;
+	Then sort the files according to specified ordering;
+	Call pdfcpu to merge files and output;
+	Clean up if necessary, and exit.
+*/
+func (m merger) merge() error {
+	pdfs, err := collectPDF(m.in)
+	if err != nil {
+		return err
+	} else if len(pdfs) < 2 {
+		fmt.Printf("Found %d PDF files under path %s. Skip merging", len(pdfs), m.in)
+		return nil
+	}
+	// in-place sort
+	m.sort(pdfs)
+	// merge
+	return m.doMerge(pdfs)
+}
+
+func isPDF(fp string) (bool, error) {
+	f, err := os.Open(fp)
+	defer f.Close()
 	if err != nil {
 		return false, err
 	}
-	if ft == TypeAbsent {
-		parent, err := m.out.parent()
-		if err != nil {
-			return false, err
-		}
-		ft, err := parent.fileType()
-		if err != nil {
-			return false, err
-		}
-		return ft == TypeDir, nil
-	}
-	if ft == TypeFile && !m.overwrite {
-		return false, nil
-	}
-	return true, nil
-}
-
-func (m merger) merge() error {
-	// read paths of files from input directory
-	// sort the files according to specified ordering
-	// call pdfcpu's merge API to merge the file
-	// output the merged file to specified destination
-}
-
-// Returns files which we infer as type PDF under the given path. Only files which are
-// the direct children of dir are inspected.
-func listPDFs(dir path) ([]path, error) {
-	ft, err := dir.fileType()
-	if err != nil {
-		return nil, err
-	}
-	if ft != TypeDir {
-		return nil, fmt.Errorf("listPDFs: path %s is not a directory", dir)
-	}
-	r, err := dir.abs()
-	if err != nil {
-		return nil, fmt.Errorf("listPDFs: error getting absolute path of %s: %w", dir, err)
-	}
-	var selected []path
-	var errWalk error
-	filepath.Walk(r, func(p string, info os.FileInfo, err error) error {
-		if err != nil {
-			errWalk = fmt.Errorf("error when walking directory %s: %w", r, err)
-			return errWalk
-		}
-		if p == r {
-			return nil
-		}
-		if info.IsDir() {
-			return filepath.SkipDir
-		}
-		// This seems to be a file, infer its type.
-		// TODO: parallelize the inferring process
-		f, err := os.Open(p)
-		defer f.Close()
-		if err != nil {
-			errWalk = fmt.Errorf("error opening file at path %s: %w", p, err)
-			return errWalk
-		}
-		head := make([]byte, HeaderLengthBytes)
-		_, err = f.Read(head)
-		if err != nil {
-			errWalk = fmt.Errorf("error reading file at path %s: %w", p, err)
-			return errWalk
-		}
-		if ftype.IsMIME(head, MIMETypePDF) {
-			selected = append(selected, path(p))
-		}
-		return nil
-	})
-	return selected, errWalk
-}
-
-func isPDF(p string) (bool, error) {
-	f, err := os.Open(p)
-	defer f.Close()
-	if err != nil {
-		return false, fmt.Errorf("isPDF: error opening file at path %s: %w", p, err)
-	}
-	head := make([]byte, HeaderLengthBytes)
+	head := make([]byte, FileHeaderLengthBytes)
 	_, err = f.Read(head)
-	if err != nil {
-		return false, fmt.Errorf("isPDF: error reading file at path %s: %w", p, err)
+	if err != nil && err != io.EOF {
+		return false, err
 	}
-	if ftype.IsMIME(head, MIMETypePDF) {
+	if filetype.IsMIME(head, MIMETypePDF) {
 		return true, nil
 	}
 	return false, nil
+}
 
+/*
+	Processes files which are the direct children of path p only. Assumes p is an absolute path.
+*/
+func collectPDF(p string) ([]pdfFile, error) {
+	var wg sync.WaitGroup
+	checked := make(chan struct {
+		f   pdfFile
+		err error
+	})
+	exit := make(chan bool)
+	filepath.Walk(p, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		} else if path == p {
+			return nil
+		} else if info.IsDir() {
+			return filepath.SkipDir
+		}
+		// path points to a file
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			ok, err := isPDF(path)
+			if err != nil || ok {
+				select {
+				case <-exit:
+				case checked <- struct {
+					f   pdfFile
+					err error
+				}{f: pdfFile{path, info.ModTime()}, err: err}:
+				}
+			}
+		}()
+		return nil
+	})
+	go func() {
+		wg.Wait()
+		// all goroutine finished
+		close(checked)
+	}()
+	defer close(exit)
+	var pdfFiles []pdfFile
+	for i := range checked {
+		if i.err != nil {
+			return nil, fmt.Errorf("Error checking type of file in %s: %w", i.f.path, i.err)
+		}
+		pdfFiles = append(pdfFiles, i.f)
+	}
+	return pdfFiles, nil
+}
+
+type pdfFile struct {
+	path    string
+	modTime time.Time
+}
+
+func (m merger) sort(pdfs []pdfFile) {
+
+}
+
+func (m merger) doMerge(pdfs []pdfFile) error {
+	return nil
 }
 
 func genPDF(n int) {
